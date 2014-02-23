b12393cdb1b1a4b932f0522cf7e345953a03ee98
Kelly Stuard <kellystuard@gmail.com>
2014-02-23 11:01:47 -0600

    Initial commit.

---
+++ Castle.Windsor.Contracts/Castle.Windsor.Contracts/IWindsorContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+
+namespace Castle.Windsor
+{
+	[ContractClass(typeof(WindsorContainerContractClass))]
+	public interface IWindsorContainer : IDisposable
+	{
+		T Resolve<T>();
+		object Resolve(Type service);
+	}
+
+	[ContractClassFor(typeof(IWindsorContainer))]
+	[ExcludeFromCodeCoverage]
+	internal abstract class WindsorContainerContractClass : IWindsorContainer
+	{
+		T IWindsorContainer.Resolve<T>()
+		{
+			Contract.Ensures(Contract.Result<T>() != null);
+			throw new NotImplementedException();
+		}
+
+		object IWindsorContainer.Resolve(Type service)
+		{
+			Contract.Ensures(Contract.Result<object>() != null);
+			throw new NotImplementedException();
+		}
+
+		void IDisposable.Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
