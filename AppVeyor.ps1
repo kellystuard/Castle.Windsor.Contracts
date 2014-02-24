b84c925d6986a913e5c620999382219a00740cdc
Kelly Stuard <kellystuard@gmail.com>
2014-02-24 17:13:40 -0600

    Testing why publish not working.

--- AppVeyor.ps1
+++ AppVeyor.ps1
@@ -8,6 +8,7 @@ $nuGetApiKeySecure = $variables["NuGetApiKeySecure"]
 
 Get-ChildItem $outFolder -Filter *.nupkg | `
 Foreach-Object {
+	Write-Output $_.FullName
 	$content = Get-Content $_.FullName
 	.\.nuget\NuGet.exe push $content
 }
