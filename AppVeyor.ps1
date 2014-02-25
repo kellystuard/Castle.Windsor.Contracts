407b41224a2da0500bd2d9a72624dbe08f8d41d2
Kelly Stuard <kellystuard@gmail.com>
2014-02-24 22:35:21 -0600

    Publishing now working.

--- AppVeyor.ps1
+++ AppVeyor.ps1
@@ -11,45 +11,9 @@ Param (
 	$projectBuildNumber
 )
 
-# list all artifacts
-foreach($artifact in $artifacts.values)
-{
-	Write-Output "Artifact: $($artifact.name)"
-	Write-Output "Type: $($artifact.type)"
-	Write-Output "Path: $($artifact.path)"
-	Write-Output "Url: $($artifact.url)"
-}
-
-# script custom variables
-foreach($name in $variables.keys)
-{
-	$value = $variables[$name]
-	Write-Output "$name=$value"
-}
-
-Write-Output $scriptPath
-Get-ChildItem $scriptPath | Format-Table -Property Name, LastWriteTime
-Write-Output $buildFolder
-Get-ChildItem $buildFolder | Format-Table -Property Name, LastWriteTime
-Write-Output $srcFolder
-Get-ChildItem $srcFolder | Format-Table -Property Name, LastWriteTime
-Write-Output $outFolder
-Get-ChildItem $outFolder | Format-Table -Property Name, LastWriteTime
-Write-Output $tempFolder
-Get-ChildItem $tempFolder | Format-Table -Property Name, LastWriteTime
-Write-Output $projectName
-Write-Output $projectVersion
-Write-Output $projectBuildNumber
-
-
-$tempSubFolder = $tempFolder + "\Castle.Windsor.Contracts"
-Write-Output $tempSubFolder
-Get-ChildItem $tempSubFolder | Format-Table -Property Name, LastWriteTime
-
 $nuGetApiKeySecure = $variables["NuGetApiKeySecure"]
 .\.nuget\NuGet.exe setApiKey $nuGetApiKeySecure
 
 $tempFolder + "\Castle.Windsor.Contracts" | Get-ChildItem -Filter "*.nupkg" | % {
-	Write-Output "Publishing " + $_.Name
 	.\.nuget\NuGet.exe push $_.FullName
 }
