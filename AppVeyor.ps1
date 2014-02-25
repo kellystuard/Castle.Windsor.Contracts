3738bb005bbae349b9c816f9987c1c852153291c
Kelly Stuard <kellystuard@gmail.com>
2014-02-24 20:04:09 -0600

    Testing why publishing not working.

--- AppVeyor.ps1
+++ AppVeyor.ps1
@@ -1,6 +1,14 @@
 Param (
 	$variables = @{},
-	$outFolder
+	$artifacts = @{},
+	$scriptPath,
+	$buildFolder,
+	$srcFolder,
+	$outFolder,
+	$tempFolder,
+	$projectName,
+	$projectVersion,
+	$projectBuildNumber
 )
 
 # list all artifacts
@@ -12,11 +20,28 @@ foreach($artifact in $artifacts.values)
 	Write-Output "Url: $($artifact.url)"
 }
 
+# script custom variables
+foreach($name in $variables.keys)
+{
+	$value = $variables[$name]
+	Write-Output "$name=$value"
+}
+
+Write-Output $scriptPath
+Write-Output $buildFolder
+Write-Output $srcFolder
+Write-Output $outFolder
+Write-Output $tempFolder
+Write-Output $projectName
+Write-Output $projectVersion
+Write-Output $projectBuildNumber
+
 $nuGetApiKeySecure = $variables["NuGetApiKeySecure"]
 .\.nuget\NuGet.exe setApiKey $nuGetApiKeySecure
 
 Write-Output $outFolder
-Get-ChildItem $outFolder | `
-Foreach-Object {
-	Write-Output $_.FullName
-}
+Get-ChildItem $scriptPath | Format-Table -Property Name, LastWriteTime
+Get-ChildItem $buildFolder | Format-Table -Property Name, LastWriteTime
+Get-ChildItem $srcFolder | Format-Table -Property Name, LastWriteTime
+Get-ChildItem $outFolder | Format-Table -Property Name, LastWriteTime
+Get-ChildItem $tempFolder | Format-Table -Property Name, LastWriteTime
